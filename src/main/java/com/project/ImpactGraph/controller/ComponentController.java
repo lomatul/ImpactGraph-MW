5d0f7e17d080b5bd03c911e715705500bac4029f
Lomatul Mahzabin <90206489+lomatul@users.noreply.github.com>
2024-08-12 14:46:41 +0600

    Delete Code added in Controller

--- src/main/java/com/project/ImpactGraph/controller/ComponentController.java
+++ src/main/java/com/project/ImpactGraph/controller/ComponentController.java
@@ -1,8 +1,12 @@
 package com.project.ImpactGraph.controller;
 
+import com.project.ImpactGraph.dto.ComponentDTO;
 import com.project.ImpactGraph.entity.Component;
 import com.project.ImpactGraph.service.ComponentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/components")
@@ -10,12 +14,58 @@ public class ComponentController {
 
     private final ComponentService componentService;
 
+    @Autowired
     public ComponentController(ComponentService componentService) {
         this.componentService = componentService;
     }
 
+    @PostMapping(path = "/create")
+    public void createComponent(@RequestBody ComponentDTO request) {
+        Component component = new Component();
+        component.setName(request.getName());
+        component.setIp(request.getIp());
+        component.setType(request.getType());
+
+        componentService.createComponent(
+                component,
+                request.getIncomingNodeIds(),
+                request.getOutgoingNodeIds()
+        );
+    }
+
+    @GetMapping(path = "/all")
+    public List<ComponentDTO> getAllComponents() {
+        return componentService.getAllComponents();
+    }
+
+    @GetMapping(path = "/ComID/{id}")
+    public Component getComponentById(@PathVariable long id) {
+        return componentService.getComponentByID(id);
+    }
+
+    @GetMapping(path = "/ComName/{name}")
+    public Component getComponentByName(@PathVariable String name) {
+        return componentService.getComponentByName(name);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public Component getComponentByName(@PathVariable Long id) {
+        return componentService.deleteComponentByID(id);
+    }
 
 
+    @PutMapping(path = "/update")
+    public Component updateComponent(@RequestBody ComponentDTO request) {
+        Component existingComponent = componentService.getComponentByID(request.getId());
 
+        existingComponent.setName(request.getName());
+        existingComponent.setIp(request.getIp());
+        existingComponent.setType(request.getType());
 
+        return componentService.updateComponent(
+                existingComponent,
+                request.getIncomingNodeIds(),
+                request.getOutgoingNodeIds()
+        );
+    }
 }
