8c20efcd2a67bef4845d785f500a420f7a1f99ea
Habib Hussain <hussainhabib05@yahoo.com>
2024-08-18 14:11:03 +0600

    add create feature

--- src/main/java/com/project/ImpactGraph/controller/ComponentController.java
+++ src/main/java/com/project/ImpactGraph/controller/ComponentController.java
@@ -1,8 +1,12 @@
 package com.project.ImpactGraph.controller;
 
+import com.project.ImpactGraph.dto.ComponentDTO;
 import com.project.ImpactGraph.entity.Component;
 import com.project.ImpactGraph.service.ComponentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/components")
@@ -10,12 +14,29 @@ public class ComponentController {
 
     private final ComponentService componentService;
 
+    @Autowired
     public ComponentController(ComponentService componentService) {
         this.componentService = componentService;
     }
 
+    @PostMapping(path = "/create")
+    public void createComponent(@RequestBody ComponentDTO request) {
+        Component component = new Component();
+        component.setName(request.getName());
+        component.setIp(request.getIp());
+        component.setType(request.getType());
 
+        componentService.createComponent(
+                component,
+                request.getIncomingNodeIds(),
+                request.getOutgoingNodeIds()
+        );
 
+    }
 
+    @GetMapping(path = "/all")
+    public List<ComponentDTO> getAllComponents(){
+        return componentService.getAllComponents();
 
+    }
 }
