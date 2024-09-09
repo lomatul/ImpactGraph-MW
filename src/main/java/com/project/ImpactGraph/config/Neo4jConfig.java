65a5ff4e9c688b234208b8e52a336ad3d808f2fc
Habib Hussain <hussainhabib05@yahoo.com>
2024-09-09 11:02:47 +0600

    update neo4j config class

--- src/main/java/com/project/ImpactGraph/config/Neo4jConfig.java
+++ src/main/java/com/project/ImpactGraph/config/Neo4jConfig.java
@@ -2,6 +2,7 @@ package com.project.ImpactGraph.config;
 
 import org.neo4j.driver.Driver;
 import org.neo4j.driver.GraphDatabase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
@@ -12,13 +13,21 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class Neo4jConfig {
 
+    @Value("${spring.neo4j.uri}")
+    private String uri;
+
+    @Value("${spring.neo4j.authentication.username}")
+    private String username;
+
+    @Value("${spring.neo4j.authentication.password}")
+    private String password;
+
     @Bean
     public Driver neo4jDriver() {
-        return GraphDatabase.driver("neo4j+s://b93c2f97.databases.neo4j.io",
-                org.neo4j.driver.AuthTokens.basic("neo4j", "eLcSToxpFiFbX9W8NG8ZnOs5WkL2PfaGtoDYJMQ0370"));
+        return GraphDatabase.driver(uri,
+                org.neo4j.driver.AuthTokens.basic(username, password));
     }
-
-    // Add this method to configure the Neo4j transaction manager
+    
     @Bean
     public PlatformTransactionManager transactionManager(Driver driver) {
         return new Neo4jTransactionManager(driver);
