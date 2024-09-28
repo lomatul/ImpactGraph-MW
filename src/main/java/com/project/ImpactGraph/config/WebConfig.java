94c6f99af90eb6104504f953e2f53fefb1eb990e
Habib Hussain <hussainhabib05@yahoo.com>
2024-09-28 08:50:37 +0600

    update application.properties

--- src/main/java/com/project/ImpactGraph/config/WebConfig.java
+++ src/main/java/com/project/ImpactGraph/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${spring.app.allowed.origins}")
+    @Value("${app.allowed.origins}")
     private String allowedOrigins;
 
     @Override
